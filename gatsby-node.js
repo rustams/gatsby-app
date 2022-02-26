2abfa2fee023af6807252e93a4e9e9c84f27fcd1
Rustam Salpagaroff <salpagaroff@gmail.com>
2022-02-26 10:38:24 +0300

    Update source

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const result = await graphql(
     `
       {
-        allContentfulBlogPost {
+        projectPage {
           nodes {
             title
             slug
@@ -27,7 +27,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  const posts = result.data.allContentfulBlogPost.nodes
+  const posts = result.data.projectPage.nodes
 
   // Create blog posts pages
   // But only if there's at least one blog post found in Contentful
